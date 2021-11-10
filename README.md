82d746b01fe60fd2392fdd499eb00fee53d156fd
QuanLeo <quanhn09041996@gmail.com>
2021-11-10 22:47:48 +0700

    Add swagger

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # nestjs-api-example
 
-## Overview 
-  - This is CRUD Api basic.
-  - Main concept:
-    - Using docker
-    - CRUD api
-    - Using swagger
-    - Using jwt-auth token
-    - Send mail
-    - Working with localstack
-##
+## Overview
+
+- This is CRUD Api basic.
+- Main concept:
+  - Using docker
+  - CRUD api
+  - Using swagger
+  - Using jwt-auth token
+  - Send mail
+  - Working with localstack
+
+## Installed
+
+- Swagger:
+  ```
+    npm install --save @nestjs/swagger swagger-ui-express
+  ```
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('TODO example')
+    .setDescription('The Todo API example')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'access_token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = +process.env.BACKEND_PORT;
   await app.listen(port);
 }
