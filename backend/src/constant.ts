d286cb05ad9876d047818469d382d0434d6324a3
QuanLeo <quanhn09041996@gmail.com>
2021-11-10 23:12:04 +0700

    Add user entity

---
+++ backend/src/constant.ts
@@ -0,0 +1,9 @@
+export enum UserRole {
+  USER = 1,
+  ADMIN,
+}
+
+export enum UserState {
+  UNACTIVE = 1,
+  ACTIVED,
+}
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,73 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { UserRole, UserState } from 'src/constant';
+
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+
+  @Exclude()
+  @Column({ nullable: true, default: '123456' })
+  password: string;
+
+  @Column({ nullable: true })
+  address?: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role?: number;
+
+  @Column('json', { nullable: true }) //for optional
+  permission: object[];
+
+  @Column({ nullable: true, default: '/default/images/default_avatar.jpg' })
+  avatar?: string;
+
+  @Column({ nullable: true })
+  dob?: string;
+
+  @Column({ nullable: true, default: null })
+  sex?: string;
+
+  @Column({ nullable: true, default: null })
+  phone_number?: string;
+
+  @Column({ nullable: true, default: 1 })
+  labor_contract?: number;
+
+  @Column({ type: 'float', nullable: true, default: 0.0 })
+  day_off?: number;
+
+  @Column({
+    type: 'enum',
+    enum: UserState,
+    default: UserState.UNACTIVE,
+  })
+  active?: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
